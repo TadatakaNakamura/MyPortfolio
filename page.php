3e235681e68a1720c242e2372195aaa61e07cead
nakamura tadataka <yu12bn144jtada@icloud.com>
2021-08-20 17:19:01 +0900

    ループ処理を追加

--- page.php
+++ page.php
@@ -1,144 +1,33 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>hamburgerメニュー</title>
-
-  <!-- CSS -->
-  <link rel="stylesheet" href="css/hamburger.css">
-
-</head>
-
-  <body>
-    <main class="l-main">
-      <article class="l-main__left">
-
-      <php? get_header(); ?>
+<?php get_header(); ?>
 
         <div class="p-contents">
 
           <div class="p-mainvisual-page">
-            <h1>ショップについて</h1>
+            <h1><?php the_title(); ?></h1>
           </div>
 
           <div class="p-article">
-            <div class="p-article__text">
-              <h2>見出しh2</h2>
-              <p>pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。</p>
-              <h3>見出しh3</h3>
-              <h4>見出しh4</h4>
-              <h5>見出しh5</h5>
-              <h6>見出しh6</h6>
-            </div>
-            <div class="p-article__quote">
-              <blockquote>Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</blockquote>
-              <p>出典元：<a href="#">〇〇〇〇〇〇〇〇〇〇〇〇</a></p>
-            </div>
-
-
-            <div class="p-picture">
-              <div class="p-picture__top">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-              </div>
-              <div class="p-picture__column top">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-              </div>
-              <div class="p-picture__column bottom">
-                <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                <img src="img/cooked-foods-750073-2.png" alt="">
-              </div>
-              <div class="p-picture__bottom">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-              </div>
-              <div class="p-picture__tile">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-                <img src="img/cooked-foods-750073-2.png" alt="">
-              </div>
-            </div>
-
-            <div class="p-article__list">
-              <ol>
-                <li>リストリストリスト</li>
-                <li>リストリストリスト
-                  <ol>
-                    <li>リスト2リスト2リスト</li>
-                    <li>リスト2リスト2リスト</li>
-                  </ol>
-                </li>
-                <li>リストリストリスト</li>
-                <li>リストリストリスト</li>
-              </ol>
-              <ul>
-                <li>リストリストリスト</li>
-                <li>リストリストリスト
-                  <ul>
-                    <li>リスト2リスト2リスト</li>
-                    <li>リスト2リスト2リスト</li>
-                  </ul>
-                </li>
-                <li>リストリストリスト</li>
-                <li>リストリストリスト</li>
-              </ul>
-            </div>
-            <pre class="p-page__code">
-              <code>
-                &lt;html&gt;
-                        &lt;head&gt;
-                        &lt;/head&gt;
-                        &lt;body&gt;
-                        &lt;/body&gt;
-                &lt;/html&gt;
-              </code>
-            </pre>
-
-            <div class="c-table">
-              <table>
-                <tr>
-                  <th>テーブル</th>
-                  <th>テーブル</th>
-                </tr>
-                <tr>
-                  <td>テーブル</td>
-                  <td>テーブル</td>
-                </tr>
-                <tr>
-                  <td>テーブル</td>
-                  <td>テーブル</td>
-                </tr>
-                <tr>
-                  <td>テーブル</td>
-                  <td>テーブル</td>
-                </tr>
-              </table>
-            </div>
-
-            <div class="c-button">
-              <button><a href="#">ボタン</a></button>
-            </div>
-
-            <div class="p-page__bold">
-              <p>boldboldboldboldboldbold</p>
-            </div>
-
+            <?php if(have_posts()):
+              while(have_posts()):
+                the_post(); ?>
+                <div id="post-<?php the_ID(); ?>"<?php post_class(); ?>>
+                  <?php the_content(); ?>
+                </div>
+                <?php endwhile;
+              else:
+                ?>
+            <p>表示する記事がありません</p>
+            <?php endif; ?>
           </div>
+
         </div>
       </article>
 
       <?php get_sidebar(); ?>
 
     </main>
-    <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
-    <script src="js/style.js"></script>
+
+    <?php wp_footer(); ?>
 
   </body>
 

8f703a9a396b5e2bc15ff70e3fbc916834598530
nakamura tadataka <yu12bn144jtada@icloud.com>
2021-08-23 23:29:04 +0900

    フッターのcssを修正

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer class="l-footer">
   <div class="l-footer__section">
-    <?php wp_nav_menu(); ?>
-    <a href="<?php get_page_link( 14 ); ?>">ショップ情報</a>
-    <a href="<?php get_page_link( 16 ); ?>">ヒストリー</a>
+    <?php wp_nav_menu( array(
+      'theme_location' => 'footer-menu'
+    )); ?>
   </div>
   <p>
     <small>Copyright : RaiseTech</small>

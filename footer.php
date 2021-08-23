96fd009a7f1a81432c4cca41f7319a7eb570e5a7
nakamura tadataka <yu12bn144jtada@icloud.com>
2021-08-23 18:53:20 +0900

    wp_nav_menuを追加

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer class="l-footer">
   <div class="l-footer__section">
+    <?php wp_nav_menu(); ?>
     <a href="<?php get_page_link( 14 ); ?>">ショップ情報</a>
     <a href="<?php get_page_link( 16 ); ?>">ヒストリー</a>
   </div>

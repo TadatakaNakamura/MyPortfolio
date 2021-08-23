ce69ff614c619cd808142b0cb8b2adeb602a6306
nakamura tadataka <yu12bn144jtada@icloud.com>
2021-08-23 18:52:33 +0900

    head内にmeta nameを追加

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="email=no,telephone=no,address=no">
+  <meta name="description" content="WordPress theme development static data for beginners">
+  <meta name="keywords" content="WordPress, Theme, development">
 
   <?php wp_head(); ?>
 

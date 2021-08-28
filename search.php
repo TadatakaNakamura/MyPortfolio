1601ea8c22edeaa1eb61a8c69e552d623b3b2255
nakamura tadataka <yu12bn144jtada@icloud.com>
2021-08-28 16:55:29 +0900

    検索機能を追加

---
+++ search.php
@@ -0,0 +1,61 @@
+<?php get_header(); ?>
+
+        <div class="p-contents">
+
+          <div class="p-mainvisual-archive">
+            <div class="p-mainvisual-archive__text">
+              <h2>Search：</h2>
+              <p><?php echo $_GET['s']; ?></p>
+            </div>
+          </div>
+
+          <div class="p-box-archivepage">
+            <h4>小見出しが入ります</h4>
+            <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+          </div>
+
+          <div class="p-menu">
+          <?php
+            if (have_posts()) :
+             while (have_posts()) :
+                the_post(); ?>
+              <div class="p-menu__card" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php the_post_thumbnail(); ?>
+                  <div class="p-menu__card__text">
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_excerpt(); ?>
+                    <a href="<?php the_permalink(); ?>">詳しく見る</a>
+                  </div>
+              </div>
+              <?php endwhile;
+              else :
+              ?><p>表示する記事がありません</p>
+              <?php endif; ?>
+            <?php
+              if (isset($_GET['s']) && empty($_GET['s'])){
+                echo '検索キーワード未入力';
+              }else{
+                echo '“'.$_GET['s'] .'”の検索結果：'.$wp_query->found_posts .'件';
+              }
+              ?>
+
+          </div>
+        </div>
+
+        <div class="p-pagenation">
+        <?php if( function_exists("the_pagination") ) the_pagination(); ?>
+        </div>
+
+      </div>
+
+      <?php get_sidebar(); ?>
+
+    </main>
+
+    <?php wp_footer(); ?>
+
+  </body>
+
+  <?php get_footer(); ?>
+
+</html>
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form class="l-header__right p-search-form" method="get" action="<?php echo home_url('/'); ?>">
-    <input class="p-search-form__box"  type="search" placeholder="" name="s" id="s">
+    <input class="p-search-form__box"  type="search" placeholder="" name="s" id="s" value="<?php if(get_search_query()) echo get_search_query() ?>">
     <input class="p-search-form__button"  type="submit" value="検索">
 </form>

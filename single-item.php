329eb9377ef64115025a5482351c72cdb33b99cd
nakamura tadataka <yu12bn144jtada@icloud.com>
2021-09-01 15:06:52 +0900

    menucardを追加

---
+++ single-item.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+
+  <?php get_header(); ?>
+
+            <div class="p-contents">
+
+              <div class="p-mainvisual-single">
+                <h1><?php the_title(); ?></h1>
+              </div>
+
+              <div class="p-article">
+              <?php
+                if(have_posts() ):
+                while( have_posts() ): the_post(); ?>
+                  <div class="p-menu-card--custom" id="post-<?php the_ID(); ?>"<?php post_class(); ?>>
+                    <?php the_post_thumbnail(); ?>
+                    <div class="p-menu-card__text--custom">
+                      <h1><?php the_title(); ?></h1>
+                      <div class='p-menu-card__info'>
+                        <p>価格：円</p>
+                        <p>アレルギー：</p>
+                      </div>
+                    </div>
+                  </div>
+                  <?php the_content();?>
+                </div>
+                <?php endwhile;
+                else: ?>
+                  <p>商品がありません</p>
+                <?php endif; ?>
+
+              </div>
+            </div>
+          </div>
+
+        <?php get_sidebar(); ?>
+      </main>
+    <?php wp_footer(); ?>
+    </body>
+  <?php get_footer(); ?>
+</html>

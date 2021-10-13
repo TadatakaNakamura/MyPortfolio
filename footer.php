917e177adaf255a7a6f24ea8c0bd50f5e90e3900
nakamura tadataka <yu12bn144jtada@icloud.com>
2021-10-13 17:11:21 +0900

    snsボタン作成

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
 <footer class="l-footer">
   <div class="l-footer__section">
   </div>
+  <ul class="c-sns-button">
+    <li><a href="TwitterのプロフィールURL" class="c-sns-button__logo c-sns-button__tw"><i class="fab fa-twitter c-sns-button__tw__fa"></i></a></li>
+    <li><a href="インスタのプロフィールURL" class="c-sns-button__logo insta_btn6"><i class="fab fa-instagram c-sns-button__tw__insta"></i></a></li>
+    <li><a href="FacebookページのURL" class="c-sns-button__logo c-sns-button__fb"><i class="fab fa-facebook-f c-sns-button__fb__fa"></i></a></li>
+  </ul>
   <p>
     <small>Copyright : tadataka</small>
   </p>
